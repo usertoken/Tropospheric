12fc694d4a2f582c216aa110b0695ee644fba325
alex Davis <alex@alex2006hw.com>
2017-12-21 14:14:27 -0800

    more-memories-01

--- src/configs/memories.js
+++ src/configs/memories.js
@@ -1,5 +1,6 @@
 const peerMemories = [
   "https://tropospheric.mybluemix.net/gun",
+  "https://tropospheric-tropospheric.193b.starter-ca-central-1.openshiftapps.com/gun",
   "https://memory02-memory02-pl.193b.starter-ca-central-1.openshiftapps.com/gun",
   "https://memory02-memory02-pl.193b.starter-ca-central-1.openshiftapps.com/gun",
   "https://memory02-memory02-alex.193b.starter-ca-central-1.openshiftapps.com/gun"
